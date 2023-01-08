949917aafaa16a81edc28d5547f3c9f1fc353e43
Roman Oberenkowski <roman.oberenkowski@student.put.poznan.pl>
2023-01-08 15:01:39 +0100

    remove old L4T (r32.3.1) kernel sources

--- kernel/kernel-4.9/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 61 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#61 SMP PREEMPT Sat Mar 21 16:09:40 CET 2020"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "paul-GV72-7RE"
-#define LINUX_COMPILER "gcc version 7.3.1 20180425 [linaro-7.3-2018.05 revision d29120a424ecfbc167ef90065c0eeb7f91977701] (Linaro GCC 7.3-2018.05) "
